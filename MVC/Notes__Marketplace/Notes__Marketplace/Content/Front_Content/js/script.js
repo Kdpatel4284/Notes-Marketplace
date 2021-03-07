43d584765d309569e9138aa87514ea82e2d27225
Kdpatel4284 <77229800+Kdpatel4284@users.noreply.github.com>
2021-03-07 23:22:37 +0530

    Add files via upload

---
+++ MVC/Notes__Marketplace/Notes__Marketplace/Content/Front_Content/js/script.js
@@ -0,0 +1,229 @@
+/*==================
+    Home page  js  
+=================*/
+function sticky_header() {
+    var header_height = jQuery('.site-header').innerHeight() / 2;
+    var scrollTop = jQuery(window).scrollTop();;
+    if (scrollTop > header_height) {
+        jQuery('body').addClass('sticky-header');
+    } else {
+        jQuery('body').removeClass('sticky-header');
+    }
+}
+
+jQuery(document).ready(function () {
+    sticky_header();
+});
+
+jQuery(window).scroll(function () {
+    sticky_header();
+});
+jQuery(window).resize(function () {
+    sticky_header();
+});
+/*==================
+   end of Home page  js  
+=================*/
+
+/*======================================
+|   |   |   |   Mobile Menu
+======================================*/
+$(function () {
+    // Show mobile nav
+    $("#mobile-nav-open-btn").click(function () {
+        $("#mobile-nav").css("height", "100%");
+    });
+    // Hide mobile nav
+    $("#mobile-nav-close-btn, #mobile-nav a").click(function () {
+        $("#mobile-nav").css("height", "0%");
+    });
+});
+
+$(".toggle-arrow").click(function () {
+    $(this).toggleClass("fa-chevron-down fa-chevron-up");
+});
+
+
+/*======================================
+|   |   |   |   Navigation
+======================================*/
+$(function () {
+    showHideNav();
+    $(window).scroll(function () {
+        //show hide nav on windows scroll
+        showHideNav();
+    });
+
+    function showHideNav() {
+        if ($(window).scrollTop() > 50) {
+            //show white nav
+            $("nav").addClass("white-nav-top");
+            //show dark logo
+            $(".h-logo img").attr("src", "/Content/Front_Content/images/pre-login/logo.png");
+            //Show back-to-top Button
+
+        } else {
+            //hide white nav 
+            $("nav").removeClass("white-nav-top");
+            $(".h-logo img").attr("src", "/Content/Front_Content/images/pre-login/top-logo.png");
+
+        }
+    }
+});
+
+/*==================
+   Search  page  js  
+=================*/
+
+// Material Select Initialization
+$(document).ready(function () {
+    $('.mdb-select').materialSelect();
+});
+
+/*==================
+end   Search  page  js  
+=================*/
+/* ================================
+        Navigation Section Is Here
+================================*/
+
+/*======================================
+|   |   |   |   Mobile Menu
+======================================*/
+$(function () {
+    // Show mobile nav
+    $("#mobile-nav-open-btn").click(function () {
+        $("#mobile-nav").css("height", "100%");
+    });
+    // Hide mobile nav
+    $("#mobile-nav-close-btn, #mobile-nav a").click(function () {
+        $("#mobile-nav").css("height", "0%");
+    });
+});
+
+$(".toggle-arrow").click(function () {
+    $(this).toggleClass("fa-chevron-down fa-chevron-up");
+});
+
+/* =============================
+        end Navigation Section Is Here
+==============================*/
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+
+function my_drop() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+/* =============================
+        FAQ Is Here
+==============================*/
+function change_faq() {
+
+    var button_text = $('#faq_expand_btn').text();
+    if (String(button_text) == String("+")) {
+        $("#faq_expand_btn").html('-');
+
+    }
+    if (String(button_text) == String("-")) {
+        $("#faq_expand_btn").html('+');
+
+    }
+}
+
+//btn 02 
+function change_faq_2() {
+
+    var button_text = $('#faq_expand_btn_2').text();
+    if (String(button_text) == String("+")) {
+        $("#faq_expand_btn_2").html('-');
+
+    }
+    if (String(button_text) == String("-")) {
+        $("#faq_expand_btn_2").html('+');
+
+    }
+}
+//btn 03 
+function change_faq_3() {
+
+    var button_text = $('#faq_expand_btn_3').text();
+    if (String(button_text) == String("+")) {
+        $("#faq_expand_btn_3").html('-');
+
+    }
+    if (String(button_text) == String("-")) {
+        $("#faq_expand_btn_3").html('+');
+
+    }
+}
+
+//btn 04 
+function change_faq_4() {
+
+    var button_text = $('#faq_expand_btn_4').text();
+    if (String(button_text) == String("+")) {
+        $("#faq_expand_btn_4").html('-');
+
+    }
+    if (String(button_text) == String("-")) {
+        $("#faq_expand_btn_4").html('+');
+
+    }
+}
+
+//btn 05 
+function change_faq_5() {
+
+    var button_text = $('#faq_expand_btn_5').text();
+    if (String(button_text) == String("+")) {
+        $("#faq_expand_btn_5").html('-');
+
+    }
+    if (String(button_text) == String("-")) {
+        $("#faq_expand_btn_5").html('+');
+
+    }
+}
+
+//btn 06 
+function change_faq_6() {
+
+    var button_text = $('#faq_expand_btn_6').text();
+    if (String(button_text) == String("+")) {
+        $("#faq_expand_btn_6").html('-');
+
+    }
+    if (String(button_text) == String("-")) {
+        $("#faq_expand_btn_6").html('+');
+
+    }
+}
+
+//btn 07 
+function change_faq_7() {
+
+    var button_text = $('#faq_expand_btn_7').text();
+    if (String(button_text) == String("+")) {
+        $("#faq_expand_btn_7").html('-');
+
+    }
+    if (String(button_text) == String("-")) {
+        $("#faq_expand_btn_7").html('+');
+
+    }
+}
